0f4686708d138fcff40ad627b5622ec2faf1d8bd
Sam Howard <howardsl1@appstate.edu>
2020-08-29 11:59:19 -0400

    initial commit

---
+++ Thingy/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Thingy
+//
+//  Created by Sam Howard on 8/25/20.
+//  Copyright © 2020 Sam Howard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
