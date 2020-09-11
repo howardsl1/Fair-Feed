da5ba057cae4a4f601698193490e527e171b8b4a
Sam Howard <howardsl1@appstate.edu>
2020-09-11 11:37:33 -0400

    bug with 'enter' in business login/signup

--- Thingy/ViewController.swift
+++ Thingy/ViewController.swift
@@ -9,14 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Customer View Text Fields
+    @IBOutlet weak var CustUlog: UITextField!
+    @IBOutlet weak var CustPlog: UITextField!
+    
+    @IBOutlet weak var CustUsign: UITextField!
+    @IBOutlet weak var CustPsign: UITextField!
+    
+    //Business View Text Fields
+    @IBOutlet weak var BusUlog: UITextField!
+    @IBOutlet weak var BusPlog: UITextField!
+    
+    @IBOutlet weak var BusUsign: UITextField!
+    @IBOutlet weak var BusPsign: UITextField!
+    //Go to Bus Review
+    @IBAction func enter(_ sender: UIButton) {
+    }
+    
+    
+    //Cust Write Review
+    @IBOutlet weak var BenterResp: UITextField!
+    @IBOutlet weak var BTview: UITextView!
+    @IBAction func setText(_ sender: UIButton) {
+        let mtext = BenterResp.text
+        BTview.text = mtext
+    }
+    @IBAction func appendText(_ sender: UIButton) {
+        let mtext = BenterResp.text
+        BTview.text += mtext ?? " "
+    }
+    
+    
+    
+    
+    //viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //CustUlog.delegate = self
+        //CustPlog.delegate = self
+        //CustUsign.delegate = self
+        //CustPsign.delegate = self
+        
+        //BusUlog.delegate = self
+        //BusPlog.delegate = self
+        //BusUsign.delegate = self
+        //BusPsign.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        
         
         
     }
+    //Was commented
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func enterTapped(_ sender: Any) {
+        
+    }
+    
+    // ERROR when tapping away from business review keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        CustUlog.resignFirstResponder()
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if ((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+            if self.view.frame.origin.y != 0 {
+                self.view.frame.origin.y = 0
+            }
+        }
+    }
 
 
+/*extension ViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textfield: UITextField) -> Bool {
+        textfield.resignFirstResponder()
+        return true
+    }*/
+    
 }
-
